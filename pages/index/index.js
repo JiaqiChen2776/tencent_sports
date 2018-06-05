84708816d2feeed785403bbf8b899fcf018dd94b
JiaqiChen2776 <171244529@qq.com>
2018-06-05 10:16:10 +0800

    初步实现'index'主页面

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,51 +4,113 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    gameList: [
+      {
+        id: '1',
+        gameDate: '6月14日',
+        gameTime: '23:00',
+        team_1: '俄罗斯',
+        team_1_pic: '../../images/shijiebei/RUS.png',
+        team_1_score: 0,
+        team_2: '沙特',
+        team_2_pic: '../../images/shijiebei/147837532.png',
+        team_2_score: 0,
+        gameLive: true,
+        isStart: false,
+        navigateUrl: '/pages/order/order'
+      },
+      {
+        id: '2',
+        gameDate: '6月15日',
+        gameTime: '20:00',
+        team_1: '埃及',
+        team_1_pic: '../../images/shijiebei/EGY.png',
+        team_1_score: 0,
+        team_2: '乌拉圭',
+        team_2_pic: '../../images/shijiebei/team_1_837.png',
+        team_2_score: 0,
+        gameLive: true,
+        isStart: false,
+        navigateUrl: '/pages/order/order'
+      },
+      {
+        id: '3',
+        gameDate: '6月15日',
+        gameTime: '23:00',
+        team_1: '摩洛哥',
+        team_1_pic: '../../images/shijiebei/MAR.png',
+        team_1_score: 0,
+        team_2: '伊朗',
+        team_2_pic: '../../images/shijiebei/0.png',
+        team_2_score: 0,
+        gameLive: true,
+        isStart: false,
+        navigateUrl: '/pages/order/order'
+      },
+    ],
+    iconList: [
+      {
+        linksPic: '../../images/wc/links_matches.png',
+        linksText: '赛程'
+      },
+      {
+        linksPic: '../../images/wc/links_goals.png',
+        linksText: '射手榜'
+      },
+      {
+        linksPic: '../../images/wc/links_table.png',
+        linksText: '积分榜'
+      },
+      {
+        linksPic: '../../images/wc/links_video.png',
+        linksText: '视频'
+      }
+    ],
+    articleList: [
+      {
+        id: '31',
+        articleTitle: '满血复活！内少迷踪步禁区1v3为所欲为 他复出就是进球机器',
+        articlePic: '../../images/shijiebei/123.jpg',
+        commentsNum: 66
+      },
+      {
+        id: '32',
+        articleTitle: '热身赛-西班牙1-1瑞士 新星破门 徳赫亚脱手送R罗进球',
+        articlePic: '../../images/shijiebei/125.jpg',
+        commentsNum: 678
+      },
+      {
+        id: '33',
+        articleTitle: '【世界杯翻译官】 圆梦1998：创造奇迹的牙买加队',
+        articlePic: '../../images/shijiebei/124.jpg',
+        commentsNum: 0
+      },
+      {
+        id: '34',
+        articleTitle: '爆切尔西为省钱将签前巴黎主帅6000万求购罗马门神',
+        articlePic: '../../images/shijiebei/120.jpg',
+        commentsNum: 5
+      },
+      {
+        id: '35',
+        articleTitle: '专访哥斯达黎家铁卫：不惧与内马尔对位 世界杯力争出线',
+        articlePic: '../../images/shijiebei/121.jpg',
+        commentsNum: 14
+      },
+      {
+        id: '36',
+        articleTitle: '博格巴：外界对我批评不公平 梅西散步就人说',
+        articlePic: '../../images/shijiebei/122.jpg',
+        commentsNum: 128
+      },
+      {
+        id: '37',
+        articleTitle: '首签？爆皇马与利物浦神锋基本谈妥 齐祖下课导致延误',
+        articlePic: '../../images/shijiebei/126.jpg',
+        commentsNum: 71
+      }
+    ]
   },
   //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  }
+  
 })
