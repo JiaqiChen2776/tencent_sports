5e81fdcd061430bd0d72c0f8a54a429d6dd1eeaf
JiaqiChen2776 <171244529@qq.com>
2018-06-05 10:07:29 +0800

    实现“赛事”主页面

--- pages/game/game.js
+++ pages/game/game.js
@@ -5,7 +5,178 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    bbSortList: [
+      {
+        id: '1',
+        bbPic:'../../images/NBA.png',
+        bbName: 'NBA',
+        navigationUrl: '/pages/order/order'
+      },
+      {
+        id: '2',
+        bbPic:'../../images/bbother.png',
+        bbName: '篮球其他',
+        navigationUrl: '/pages/order/order'
+      },
+      {
+        id: '3',
+        bbPic:'../../images/CBA.png',
+        bbName: 'CBA',
+        navigationUrl: '/pages/order/order'
+      },
+      {
+        id: '4',
+        bbPic:'../../images/NCAA.png',
+        bbName: 'NCAA',
+        navigationUrl: '/pages/order/order'
+      },
+    ],
+    fbSortList: [
+      {
+        id: '11',
+        bbPic:'../../images/shijiebei.png',
+        bbName: '世界杯',
+        navigationUrl: '/pages/order/order'
+      },
+      {
+        id: '12',
+        bbPic:'../../images/zhongchao.png',
+        bbName: '中超',
+        navigationUrl: '/pages/order/order'
+      },
+      {
+        id: '13',
+        bbPic:'../../images/yaguan.png',
+        bbName: '亚冠',
+        navigationUrl: '/pages/order/order'
+      },
+      {
+        id: '14',
+        bbPic:'../../images/ouguan.png',
+        bbName: '欧冠',
+        navigationUrl: '/pages/order/order'
+      },
+      {
+        id: '15',
+        bbPic:'../../images/yingchao.png',
+        bbName: '英超',
+        navigationUrl: '/pages/order/order'
+      },
+      {
+        id: '16',
+        bbPic:'../../images/yijia.png',
+        bbName: '意甲',
+        navigationUrl: '/pages/order/order'
+      },
+      {
+        id: '17',
+        bbPic:'../../images/xijia.png',
+        bbName: '西甲',
+        navigationUrl: '/pages/order/order'
+      },
+      {
+        id: '18',
+        bbPic:'../../images/oulianbei.png',
+        bbName: '欧联杯',
+        navigationUrl: '/pages/order/order'
+      },{
+        id: '19',
+        bbPic:'../../images/dejia.png',
+        bbName: '德甲',
+        navigationUrl: '/pages/order/order'
+      },
+      {
+        id: '20',
+        bbPic:'../../images/fajia.png',
+        bbName: '法甲',
+        navigationUrl: '/pages/order/order'
+      },
+      {
+        id: '21',
+        bbPic:'../../images/suchao.png',
+        bbName: '苏超',
+        navigationUrl: '/pages/order/order'
+      },
+      {
+        id: '22',
+        bbPic:'../../images/echao.png',
+        bbName: '俄超',
+        navigationUrl: '/pages/order/order'
+      },{
+        id: '23',
+        bbPic:'../../images/hejia.png',
+        bbName: '荷甲',
+        navigationUrl: '/pages/order/order'
+      },
+      {
+        id: '24',
+        bbPic:'../../images/puchao.png',
+        bbName: '葡超',
+        navigationUrl: '/pages/order/order'
+      },
+      {
+        id: '25',
+        bbPic:'../../images/tuchao.png',
+        bbName: '土超',
+        navigationUrl: '/pages/order/order'
+      },
+      {
+        id: '26',
+        bbPic:'../../images/aochao.png',
+        bbName: '澳超',
+        navigationUrl: '/pages/order/order'
+      },
+      {
+        id: '27',
+        bbPic:'../../images/ftother.png',
+        bbName: '足球其他',
+        navigationUrl: '/pages/order/order'
+      },
+      {
+        id: '28',
+        bbPic:'../../images/cfacup.png',
+        bbName: '足协杯',
+        navigationUrl: '/pages/order/order'
+      }
+    ],
+    otherList: [
+      {
+        id: '31',
+        bbPic:'../../images/143646851.png',
+        bbName: '法网',
+        navigationUrl: '/pages/order/order'
+      },
+      {
+        id: '32',
+        bbPic:'../../images/NHL.png',
+        bbName: 'NHL',
+        navigationUrl: '/pages/order/order'
+      },
+      {
+        id: '33',
+        bbPic:'../../images/esports.png',
+        bbName: '电竞',
+        navigationUrl: '/pages/order/order'
+      },
+      {
+        id: '34',
+        bbPic:'../../images/147359734.png',
+        bbName: 'F1',
+        navigationUrl: '/pages/order/order'
+      },
+      {
+        id: '35',
+        bbPic:'../../images/NFL.png',
+        bbName: 'NFL',
+        navigationUrl: '/pages/order/order'
+      },
+      {
+        id: '36',
+        bbPic:'../../images/zonghe.png',
+        bbName: '综合',
+        navigationUrl: '/pages/order/order'
+      }
+    ]
   },
 
   /**
