af6dc4eb1573bd4d1cdb8b65e11a78f14b80ad59
JiaqiChen2776 <171244529@qq.com>
2018-06-14 06:44:34 +0800

    ''

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 //app.js
+import articleList from './utils/articleList.js';
+import dayList from './utils/dayList.js';
+import nbaTeamRank from './utils/nbaTeamRank.js';
+import nbaPlayerRank from './utils/nbaPlayerRank.js';
+import gameList from './utils/gameList.js';
+import wcMatchesList from './utils/wcMatchesList.js';
 App({
   onLaunch: function () {
+    Object.assign(this.globalData,articleList);
+    Object.assign(this.globalData,dayList);
+    Object.assign(this.globalData,nbaTeamRank);
+    Object.assign(this.globalData,nbaPlayerRank);
+    Object.assign(this.globalData,gameList);
+    Object.assign(this.globalData,wcMatchesList);
     wx.getStorage({
       key: 'user-info',
       success:(res) => {
