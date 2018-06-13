0d36e0584237fcd32376ea9f6d8ae72cad96c2d5
JiaqiChen2776 <171244529@qq.com>
2018-06-14 06:14:08 +0800

    '解决了热门页的赛事日期切换问题'

--- pages/hot/hot.js
+++ pages/hot/hot.js
@@ -1,41 +1,105 @@
 // pages/hot/hot.js
+const app = getApp()
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
+    swiperCurIndex: 2,
     month: '',
-    day: ''
+    day: '',
+    week: '',
+    i: 0,
+    weekArr: ['周日','周一','周二','周三','周四','周五','周六'],
+    curDate: '',
+    daysCountArr: [// 保存各个月份的长度，平年
+      31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31
+    ],
+    v_live: true,
+    vLive_pic: '../../images/vLive.png',
+    vLive_text:'视频直播',
+    pLive_pic: '../../images/pLive.png',
+    pLive_text:'图文直播',
+    dayList: []
+  },
+
+  onLoad: function (options) {
+    this.setData({
+      dayList: app.globalData.dayList
+    })
+    console.log(this.data.dayList);
+    // console.log(dayList[0].gameDate);
+    // console.log(dayList[0].gameList);
+  },
+
+
+  changeMatch(e) {
+    const current = e.detail.current;//4  3
+    const befInd = this.data.swiperCurIndex;  //3
+    const index = current - befInd;
+    
+    if (index <= -1) {
+      this.preDay();
+
+    } else if (index >= 1) {
+      this.nextDay();
+    } else {
+      return
+    }
+    this.setData({
+      swiperCurIndex: current,
+    })
   },
 
   preDay() {
     let day = this.data.day;
+    let month = this.data.month;
+    let week= this.data.week;
+    let i = this.data.i;
+    if (i<=0) {
+      i = 6;
+    }else {
+      i--;
+    }
+    if(day<=1) {
+      month--;
+      day = this.data.daysCountArr[month-1];
+    }else {
+      day--;
+    }
+    // console.log(month+'月'+day+'日'+' '+this.data.weekArr[i]);
     this.setData({
-      day: day-1
+      month,
+      day,
+      i,
+      week: this.data.weekArr[i],
+      curDate: month+'月'+day+'日'+' '+this.data.weekArr[i]
     })
   },
 
   nextDay() {
     let day = this.data.day;
-    this.setData({
-      day: day+1
-    })
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    let date = new Date;
-    let month = date.getMonth() + 1;
-    let day = date.getDate();
+    let month = this.data.month;
+    let week= this.data.week;
+    let i = this.data.i;
+    if (i>=6) {
+      i = 0;
+    }else {
+      i++;
+    }
+    if (day>=this.data.daysCountArr[month-1]) {
+      day = 1;
+      month++;
+    }else {
+      day++;
+    }
     this.setData({
       month,
-      day
+      day,
+      i,
+      week: this.data.weekArr[i],
+      curDate: month+'月'+day+'日'+' '+this.data.weekArr[i]
     })
   },
 
+  
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
@@ -47,7 +111,17 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+    let date = new Date;
+    let month = date.getMonth() + 1;
+    let day = date.getDate();
+    let i = date.getDay();
+    this.setData({
+      month,
+      day,
+      i,
+      week: this.data.weekArr[i],
+      curDate: month+'月'+day+'日'+' '+this.data.weekArr[i]
+    })
   },
 
   /**
