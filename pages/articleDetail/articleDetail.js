c1eee2db3b74ffb68f9089747108d1b428c9c154
JiaqiChen2776 <171244529@qq.com>
2018-06-14 06:21:20 +0800

    '实现文章详情页'

---
+++ pages/articleDetail/articleDetail.js
@@ -0,0 +1,69 @@
+// pages/articleDetail/articleDetail.js
+const app = getApp()
+Page({
+
+  data: {
+    article: null
+  },
+
+  onLoad: function (options) {
+    const id = options.id;
+    // console.log(id);
+    const article = app.globalData.articleList.filter((item) => {
+      return item.id == id
+    })
+    console.log(article);
+    this.setData({
+      article: article[0]
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
